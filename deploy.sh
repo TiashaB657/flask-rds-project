5157b48eafc414b4ddc80b23dbbaf90e76565abc
EC2 Default User <ec2-user@ip-172-31-1-30.ec2.internal>
2026-03-20 07:36:10 +0000

    setup ci/cd

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,20 @@
 
 cd /home/ec2-user/flask-rds-project
 
-git pull origin main
+# Create venv if not exists
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
 
+# Activate venv
 source venv/bin/activate
 
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-sudo systemctl restart nginx
+# Kill old gunicorn if running
+pkill gunicorn || true
 
+# Start app
+nohup gunicorn -w 4 -b 0.0.0.0:8080 app:app > output.log 2>&1 &
